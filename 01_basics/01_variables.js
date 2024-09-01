1883a9f698eb937dd1eea44aeb13d2ba06bffe8f
ajinkya kokane <94737218+ajinkyakokane@users.noreply.github.com>
2024-09-01 21:38:33 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accoountId = 144553
+let accountEmail = "ajinkya@gamil.com"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+let accountState;
+
+// accountId = 12346 //not allowed
+
+accountEmail = "aj@gmail.com"
+accountPassword = "2121221212"
+accountCity = "Pune" 
+
+// perfer not to use var
+// because of issue in block scope and functional scope
+
+console.table([accoountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alart(3 + 3)//we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability shoild be high
+
+console.log("Ajinkya")
+
+let name  = "Ajinkya"
+let age = 18 
+let isLoggedImn = false
+
+//number => 2 to power 53
+// bigint treading
+// string => "" 
+// boolean => trur/false
+// null => standalone value
+// undefined => 
+// symbol => uniqe 
+
+//object
+
+console.log(typeof null) //type of null is object
+console.log(typeof undefined) //type of undefined is undefined type
+
+ 
